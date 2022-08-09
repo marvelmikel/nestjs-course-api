08d2c53b732f15afcf56f25b17fc48074b2c4776
marvelmikel <marvelmikel1997@gmail.com>
2022-08-09 03:26:08 -0700

    first commit

---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,4 @@
+export const COURSES = [
+    { id: 1, title:'Nodesj- the complete guide', description:"master nodejs", author:'marvel', url:'https://humucarecleaningtraining.zoeplusprojects.com/courses/11/how-to-work-in-the-warehouse'},
+    { id: 2, title:'Reactjs- the complete guide', description:"master nodejs", author:'marvel', url:'https://humucarecleaningtraining.zoeplusprojects.com/courses/11/how-to-work-in-the-warehouse'},
+];
